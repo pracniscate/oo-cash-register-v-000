deb7648a8f19634cdfeab91a7134af425269804b
Lorena <lorena.giddens@outlook.com>
2018-06-14 14:09:22 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,9 @@
 class CashRegister
   attr_accessor :total
 
-  # def initialize(total)
-  #   # @total = total
-  #   @total = 0
-  # end
+  def initialize(total)
+    @total = total
+  end
 
   def new
     @total = 0
